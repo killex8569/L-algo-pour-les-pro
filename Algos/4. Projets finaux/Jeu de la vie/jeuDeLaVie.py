19ed0c2829d54e33d8814f978f5e246e72acc4b3
killex8569 <alexandreanette@gmail.com>
2026-03-15 08:58:24 +0100

    Feat: Starting jeu de la vie

---
+++ Algos/4. Projets finaux/Jeu de la vie/jeuDeLaVie.py
@@ -0,0 +1,16 @@
+class JDLV:
+    def __init__(self):
+        self.n_grille = 30
+        self.nb_cell = 6
+        self.grp_cell = 2
+
+    def SetParameter(self, taille_grille, nb_cell, ):
+        pass
+
+    def create_grille(self, n):
+        grille = []
+        for i in range(n):
+            for j in range(n):
+                grille
+
+
